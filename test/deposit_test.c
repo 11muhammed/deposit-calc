2978db574165b4f5167a58fc5091f6c8a4b45955
Абдуллоев Мухаммаджон Муминджонович <ip611s01@mwp06.csc.local>
2017-05-03 12:05:40 +0700

    232

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -24,29 +24,6 @@ CTEST(cond, test6){
     ASSERT_EQUAL(0,conditions(1000,-6));
 }
 CTEST(cond, test7){
-   ASSERT_EQUAL(1,conditions(1000,390));
+   ASSERT_EQUAL(1,conditions(1000,330));
 }
 
-//Check function calc
-
-CTEST(calct, test1){
-    ASSERT_EQUAL(900,calc(1000,1));
-}
-CTEST(calct, test2){
-    ASSERT_EQUAL(1020,calc(1000,31));
-}
-CTEST(calct, test3){
-    ASSERT_EQUAL(1060,calc(1000,121));
-}
-CTEST(calct, test4){
-    ASSERT_EQUAL(1120,calc(1000,241));
-}
-CTEST(calct, test5){
-    ASSERT_EQUAL(90000,calc(100000,1));
-}
-CTEST(calct, test6){
-    ASSERT_EQUAL(103000,calc(100000,31));
-}
-CTEST(calct, test7){
-    ASSERT_EQUAL(108000,calc(100000,121));
-}
